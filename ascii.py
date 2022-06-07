ff0c521d234323a0fa48f070ec965bc8d8db69d6
Rakesh Roy <rakesh@appycodes.com>
2022-06-07 10:31:46 +0530

    New Pattern added

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
-i = random.randrange(9) + 1 
+i = random.randrange(10) + 1 
 website="https://raw.githubusercontent.com/rakeshappycodes/ubuntu-terminal-splash/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
