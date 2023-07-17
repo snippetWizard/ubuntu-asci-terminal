0dd572cdf7cffecffdf90957ec02a23089826501
Vicky Paul <68845437+Sagar-codes@users.noreply.github.com>
2023-07-18 00:01:16 +0530

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
 i = random.randrange(10) + 1 
-website="https://raw.githubusercontent.com/rakeshappycodes/ubuntu-terminal-splash/master/art/"+ str(i) + ".txt"
+website="https://raw.githubusercontent.com/sagar-codes/ubuntu-terminal-splash/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
